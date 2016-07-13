bb3a6bb75b8f47b5ec14b2825db7fe27dfb9ad4a
OSIDT <ssqosidt@gmail.com>
2016-07-13 15:13:40 +1000

    Controller.py - Event Label not being populated because id not present in request dictionary, check for id but also resource_id - Added extra checks to ensure an accurate event label is passed - Changed language around 'id' to be event_label to better describe how the variable is being used - Passing full request parameters to google analytic hits

--- ckanext/googleanalytics/controller.py
+++ ckanext/googleanalytics/controller.py
@@ -30,7 +30,7 @@ class GAController(BaseController):
 class GAApiController(ApiController):
     # intercept API calls to record via google analytics
     def _post_analytics(
-            self, user, request_obj_type, request_function, request_id):
+            self, user, request_obj_type, request_function, request_event_label, request_dict={}):
         if config.get('googleanalytics.id'):
             data_dict = {
                 "v": 1,
@@ -43,8 +43,16 @@ class GAApiController(ApiController):
                 "dr": c.environ.get('HTTP_REFERER', ''),
                 "ec": "CKAN API Request",
                 "ea": request_obj_type+request_function,
-                "el": request_id,
+                "el": request_event_label
             }
+            #Rename keys in request_dict to not conflict with GA
+            request_dict_keys = request_dict.keys()
+            if len(request_dict_keys) > 0:
+                for request_key in request_dict_keys:
+                    prefix = 'ckan_'
+                    request_dict[prefix + request_key] = request_dict.pop(request_key)
+            data_dict.update(request_dict)
+                
             plugin.GoogleAnalyticsPlugin.analytics_queue.put(data_dict)
 
     def action(self, logic_function, ver=None):
@@ -54,12 +62,18 @@ class GAApiController(ApiController):
             request_data = self._get_request_data(
                 try_url_params=side_effect_free)
             if isinstance(request_data, dict):
-                id = request_data.get('id', '')
-                if 'q' in request_data:
-                    id = request_data['q']
-                if 'query' in request_data:
-                    id = request_data['query']
-                self._post_analytics(c.user, logic_function, '', id)
+                event_label = request_data.get('id', '')
+                if event_label == '':
+                    event_label = request_data.get('resource_id', '')
+                if event_label == '' and 'q' in request_data:
+                    event_label = 'Query'
+                if event_label == '' and 'query' in request_data:
+                    event_label = 'Query'
+                if event_label == '' and 'sql' in request_data:
+                    event_label = 'SQL Query'
+                if event_label == '':
+                    event_label = logic_function
+                self._post_analytics(c.user, logic_function, '', event_label, request_data)
         except Exception, e:
             log.debug(e)
             pass
